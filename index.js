92d2c404ee2a41427d178df92cfc35caeafec86f
Alexander Santos <alexsantosantana@live.com>
2019-08-20 20:20:45 -0400

    inital commit

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-var express = require('express');
+//Insert modules needed for the router
+const express = require('express');
+const fs = require('fs')
+const path = require('path')
+const ejs = require('ejs');
+const app = express();
 
-var app = express();
 
 
+//Middleware set up EJS & static
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+
+//Routes
 app.get('/', (req, res, next) => {
-  res.send('Hello World');
-})
+  res.json('Hello World');
+});
 
 
-var PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Server running on port 3000');
 });
