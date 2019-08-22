63e231ab406134c9ef50a10d7ffdef0966f7847f
Juan Castillo <castillojuan1000@gmail.com>
2019-08-22 18:46:17 -0400

    created signUP route and template

--- index.js
+++ index.js
@@ -34,21 +34,28 @@ app.set('views', './views');
 
 
 //Routes
-app.get('/', (req, res, next) => {
-  res.send('Hello World');
+app.get('/signup', (req, res, next) => {
+  if (req.session.user_id !== undefined) {
+    res.redirect("/home");
+    return;
+  }
+  res.render('signup');
 });
 
 app.post('/signup', (req, res, next) => {
   var email = req.body.email;
   var password = req.body.password;
   var name = req.body.name;
-  bcrypt.hash(password, 10, (err, hash) => {// this allows the password to be private
+  bcrypt.hash(password, 10, (err, hash) => {//! this allows the password to be private 
     db.user.create({ name: name, email: email, password_hash: hash }).then((user) => {
+
+      //*creating a session with the user's id
       req.session.user_id = user.id;
-      res.redirect('/where?')
+      res.redirect('/home')
     })
   })
 })
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Server running on port 3000');
