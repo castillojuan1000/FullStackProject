4650f186b71d2269ace5eb84e1e6f13f9676400d
Alexander Santos <alexsantosantana@live.com>
2019-08-24 13:33:17 -0400

    Fixiing sequelize modeling bugs

--- models/categories.js
+++ models/categories.js
@@ -1,42 +1,43 @@
 /* jshint indent: 1 */
 
 module.exports = function (sequelize, DataTypes) {
-  const categories = sequelize.define('categories', {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-      field: 'id'
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: 'name'
-    },
-    surveysId: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      references: {
-        model: 'surveys',
-        key: 'id'
-      },
-      field: 'surveysId'
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      field: 'createdAt'
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      field: 'updatedAt'
-    }
-  }, {
-      tableName: 'categories'
-    });
-  categories.associate = function (models) {
-    categories.belongsTo(models.surveys, { foreignKey: 'surveysId' })
-  }
+	const categories = sequelize.define('categories', {
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+			field: 'id'
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: true,
+			field: 'name'
+		},
+		surveysId: {
+			type: DataTypes.INTEGER,
+			allowNull: true,
+			references: {
+				model: 'surveys',
+				key: 'id'
+			},
+			field: 'surveysId'
+		},
+		createdAt: {
+			type: DataTypes.DATE,
+			allowNull: false,
+			field: 'createdAt'
+		},
+		updatedAt: {
+			type: DataTypes.DATE,
+			allowNull: false,
+			field: 'updatedAt'
+		}
+	}, {
+			tableName: 'categories'
+		});
+	categories.associate = function (models) {
+		categories.belongsTo(models.surveys, { foreignKey: 'surveysId' })
+	}
+	return categories
 };
