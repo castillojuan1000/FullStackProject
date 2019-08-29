20b700bece21ba8ee2558f6b773a3ba353c2185c
Juan Castillo <castillojuan1000@gmail.com>
2019-08-29 18:17:33 -0400

    fixed active btns

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ myStore.sync();
 app.use(bodyParser.urlencoded({ extended: false }))
 
 //Middleware set up EJS & static
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '/public/')))
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
