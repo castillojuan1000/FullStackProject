ab890bcf018d10ce4d91c8be3f51f7999d15f870
Alexander Santos <alexsantosantana@live.com>
2019-08-29 20:45:24 -0400

    staging my code to pull juan's

--- public/js/startButton.js
+++ public/js/startButton.js
@@ -1,17 +1,19 @@
 const startButton = document.getElementById('startSurvey')
+
 startButton.addEventListener('click', () => {
     axios.get('/surveyData').then(response => {
-        const survey = response.data
+        const survey = response.data;
         var main = document.getElementsByClassName('container')[0]
-        main.innerHTML = survey.map(question => {
+        main.innerHTML = survey.map(Question => {
+            const { type } = Question
             //* This switch case is checking our question type {String}
             //* We return rendered html based on the question type.
-            switch (question.type) {
-                case 'checkbox': return checkBoxRender(question)
+            switch (type) {
+                case 'checkbox': return checkBoxRender(Question)
                     break;
-                case 'radio': return readioRender(question)
+                case 'radio': return readioRender(Question)
                     break;
-                case 'input': return inputRender(question)
+                case 'input': return inputRender(Question)
                     break;
             }
         }).join("")
@@ -27,19 +29,20 @@ startButton.addEventListener('click', () => {
  */
 
 function checkBoxRender(Question) {
-    const choices = Question.choices.map(choice => {
+    const { question, type, choices } = Question;
+    const choicesHTML = choices.map(choice => {
         return `<div class="row">
         <div class="col-8 survey-options">
             <div class="form-check">
                 <input class="form-check-input" type="checkbox" value="" id="firstbox">
                     <label class="form-check-label" for="firstbox">
-                        ${choice}
+                        ${choicesHTML}
             </label>
         </div>`
     })
     return ` <div class="row">
         <div class="col-8 survey-question">
-            <h2>${Question.question}</h2>
+            <h2>${question}</h2>
         </div>
         ${choices.join("")}
         <button type = "button" class= "btn btn-primary" > Next</button >
@@ -52,19 +55,18 @@ function checkBoxRender(Question) {
  * @returns {String}: HTML rendering for survey question
  */
 function readioRender(Question) {
-    const choices = Question.choices.map(choice => {
-        return `        
-        <input type="radio" value=""> ${choice}</input>
-`
+    const { question, type, choices } = Question;
+    const choicesHTML = choices.map(choice => {
+        return ` <input type="radio" value=""> ${choice}</input>`
     })
-    const renderedHTML = `      <div class="row">
+    const renderedHTML = `<div class="row">
             <div class="col-8 survey-question">
-                <h2>${Question.question}</h2>
+                <h2>${question}</h2>
             </div>
         </div>
         <div class="row">
         <div class="col-8 survey-options">
-        ${choices.join("")}
+        ${choicesHTML.join("")}
         <button type="button" class="btn btn-primary">Next</button>
         </div>
         </div>`
@@ -77,19 +79,20 @@ function readioRender(Question) {
  * @returns {String}: HTML rendering for survey question
  */
 function inputRender(Question) {
-    const choices = Question.choices.map(choice => {
+    const { question, type, choices } = Question;
+    const choicesHTML = choices.map(choice => {
         return `${choice}: <input type="text" name="${choice}" value=""><br></br>`
     })
     const renderedHTML = `
     <div class="row">
     <div class="col-8 survey-question">
-        <h2>${Question.question}</h2>
+        <h2>${question}</h2>
     </div>
 </div>
 <div class="row">
             <div class="col-8 survey-options">
                     <form action=" ">
-                    ${choices.join("")}
+                    ${choicesHTML.join("")}
                     <button type="button" class="btn btn-primary">Next</button>
                     </form>
             </div>
