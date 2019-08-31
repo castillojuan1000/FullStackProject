836b58eaf3ad6a57327c2c1cacb6e33c73b8ac93
Alexander Santos <alexsantosantana@live.com>
2019-08-31 13:58:16 -0400

    commenting for directions

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 //Insert modules needed for the router
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const ejs = require('ejs');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const db = require('./models');
-const crypto = require('crypto');
-const nodemailer = require('nodemailer');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const ejs = require("ejs");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const db = require("./models");
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
 const app = express();
-const surveyJSON = require('./surveySet');
-
+const surveyJSON = require("./surveySet");
 
 //* Amazon aws
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_KEY
-})
+});
 
 //Connect sequelize session to our sequelize db
 const myStore = new SequelizeStore({
@@ -27,251 +26,272 @@ const myStore = new SequelizeStore({
 });
 
 //set the store to myStore where we connect the DB details
-app.use(session({
-  secret: 'mySecret',
-  resave: false,
-  saveUninitialized: true,
-  store: myStore
-}));
+app.use(
+  session({
+    secret: "mySecret",
+    resave: false,
+    saveUninitialized: true,
+    store: myStore
+  })
+);
 
 myStore.sync();
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //Middleware set up EJS & static
-app.use(express.static(path.join(__dirname, '/public/')))
-app.set('view engine', 'ejs');
-app.set('views', './views');
-
+app.use(express.static(path.join(__dirname, "/public/")));
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 /* 
   !GET ROUTES
   TODO: Implement all of the GET routes
 */
-app.get('/', (req, res, next) => {
-  res.redirect('/welcome');
-})
-
-app.get('/welcome', (req, res, next) => {
-  if (req.session.userId !== undefined) {
-    res.render('welcome', { 'isAuthenticated': true })
-  } else {
-    res.render('welcome', { 'isAuthenticated': false })
-  }
-})
-
-app.get('/home', (req, res, next) => {
-  res.render('home');
+app.get("/", (req, res, next) => {
+  res.redirect("/welcome");
 });
 
-app.get('/login', (req, res, next) => {
-  res.render('login', { error_message: '' })
-})
+app.get("/welcome", (req, res, next) => {
+  if (req.session.userId !== undefined) {
+    res.render("welcome", { isAuthenticated: true });
+  } else {
+    res.render("welcome", { isAuthenticated: false });
+  }
+});
 
-app.get('/signup', (req, res) => {
-  if (req.session.userId !== undefined) { // check and see if the user has userID
+app.get("/home", (req, res, next) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res, next) => {
+  res.render("login", { error_message: "" });
+});
+
+app.get("/signup", (req, res) => {
+  if (req.session.userId !== undefined) {
+    // check and see if the user has userID
     res.redirect("/profile");
     return;
   }
-  res.render('signup', { error: '' }) // this allows the request to be sent back to the user 
-})
-
-
+  res.render("signup", { error: "" }); // this allows the request to be sent back to the user
+});
 
 //******* Sign Out Route ********************
-app.get('/signOut', (req, res, next) => {
-  req.session.destroy()
-  res.redirect('/login')
-})
+app.get("/signOut", (req, res, next) => {
+  req.session.destroy();
+  res.redirect("/login");
+});
 
-app.get('/survey', (req, res, next) => {
-  res.render('survey')
-})
-app.get('/surveyData', (req, res, next) => {
-  res.json(surveyJSON)
-})
-
-
+app.get("/survey", (req, res, next) => {
+  res.render("survey");
+});
+app.get("/surveyData", (req, res, next) => {
+  res.json(surveyJSON);
+});
 
 //******* User Profile Routes ********************
-app.get('/userprofile', (req, res, next) => {
+app.get("/userprofile", (req, res, next) => {
   user_id = req.session.userId;
-  db.users.findByPk(user_id).then((user) => {
-    console.log(user.phoroUrl)
-    const { name, email, photo_url } = user
-    res.render('userprofile', {
+  db.users.findByPk(user_id).then(user => {
+    console.log(user.phoroUrl);
+    const { name, email, photo_url } = user;
+    res.render("userprofile", {
       name: name,
       email: email,
       id: user_id,
       photo: photo_url
     });
-  })
+  });
+});
 
-})
+app.get("/reset", (req, res) => {
+  db.users
+    .findOne({
+      where: {
+        resetPasswordToken: req.query.token
+      }
+    })
+    .then(user => {
+      if (user == null) {
+        res.json("Password reset link is invalid or has expired");
+      } else {
+        req.session.userId = user.id;
+        res.render("reset", { email: user.email });
+      }
+    });
+});
 
-app.get('/reset', (req, res) => {
-  db.users.findOne({
-    where: {
-      resetPasswordToken: req.query.token,
-    }
-  }).then(user => {
-    if (user == null) {
-      res.json('Password reset link is invalid or has expired')
-    } else {
-      req.session.userId = user.id
-      res.render('reset', { email: user.email })
-    }
-  })
-})
-
-app.get('/forgotPassword', (req, res) => {
-  res.render('forgotPass', { message: '' })
-})
-
+app.get("/forgotPassword", (req, res) => {
+  res.render("forgotPass", { message: "" });
+});
 
 //! POST ROUTES
-app.post('/forgotPassword', (req, res) => {
-  console.log(req.body.email)
-  if (req.body.email === '') {
-    res.render('forgotPass', { message: 'Please enter an email.' })
+app.post("/forgotPassword", (req, res) => {
+  console.log(req.body.email);
+  if (req.body.email === "") {
+    res.render("forgotPass", { message: "Please enter an email." });
   } else {
-    db.users.findOne({ where: { email: req.body.email } }).then(async (user) => {
-      if (user.email === undefined) {
-        res.render('forgotPass', { message: "I couldn't find that email in my records" })
-      } else {
-        const token = crypto.randomBytes(20).toString('hex');
-        user.update({
-          resetPasswordToken: token
-        })
-        const transporter = nodemailer.createTransport({
-          host: 'smtp.sendgrid.net',
-          port: 587,
-          auth: {
-            user: 'apikey',
-            pass: process.env.SENDGRIDAPIKEY
-          }
-        });
-        const mailOptions = {
-          from: 'noreply@giftToYou.ga',
-          to: req.body.email,
-          subject: 'GiftToYou password reset',
-          html: `<p>You are receiving this because your (or someone else) has requested the pasword for your account
+    db.users
+      .findOne({ where: { email: req.body.email } })
+      .then(async user => {
+        if (user.email === undefined) {
+          res.render("forgotPass", {
+            message: "I couldn't find that email in my records"
+          });
+        } else {
+          const token = crypto.randomBytes(20).toString("hex");
+          user.update({
+            resetPasswordToken: token
+          });
+          const transporter = nodemailer.createTransport({
+            host: "smtp.sendgrid.net",
+            port: 587,
+            auth: {
+              user: "apikey",
+              pass: process.env.SENDGRIDAPIKEY
+            }
+          });
+          const mailOptions = {
+            from: "noreply@giftToYou.ga",
+            to: req.body.email,
+            subject: "GiftToYou password reset",
+            html: `<p>You are receiving this because your (or someone else) has requested the pasword for your account
           to be reset. Please click the following link to complete the proccess and reset your password.<br> <a href="http://localhost:3000/reset/?token=${token}">Click here</a><br><br>
           If you did not request this, please ignore this email and your password will not be changed.</p>`
+          };
+          await transporter.sendMail(mailOptions, function(err, res) {
+            if (err) {
+              console.error("there was an error: ", err);
+            } else {
+              console.log("Sent Email");
+            }
+          });
         }
-        await transporter.sendMail(mailOptions, function (err, res) {
-          if (err) {
-            console.error('there was an error: ', err)
-          } else {
-            console.log('Sent Email')
-          }
-        })
-      }
-    }).then(() => {
-      res.render('forgotPass', { message: 'Recovery email sent.' })
-    }).catch(() => {
-      res.render('forgotPass', { message: 'Email not found!' })
-    })
+      })
+      .then(() => {
+        res.render("forgotPass", { message: "Recovery email sent." });
+      })
+      .catch(() => {
+        res.render("forgotPass", { message: "Email not found!" });
+      });
   }
-})
-app.post('/signup', (req, res, next) => {
+});
+app.post("/signup", (req, res, next) => {
   req.body.email.toLowerCase();
   var password = req.body.password;
   var name = req.body.name;
-  bcrypt.hash(password, 10, (err, hash) => {// this allows the password to be private
-    db.users.create({ name: name, email: email, passwordHash: hash }).then((user) => {
-      req.session.userId = user.id;
-      res.redirect('/')
-    }).catch(err => {
-      res.render('signup', { error: 'This email already exist' })
-    })
-  })
-})
+  bcrypt.hash(password, 10, (err, hash) => {
+    // this allows the password to be private
+    db.users
+      .create({ name: name, email: email, passwordHash: hash })
+      .then(user => {
+        req.session.userId = user.id;
+        res.redirect("/");
+      })
+      .catch(err => {
+        res.render("signup", { error: "This email already exist" });
+      });
+  });
+});
 
-
-app.post('/login', (req, res, next) => {
+app.post("/login", (req, res, next) => {
   const email = req.body.email.toLowerCase();
-  const password = req.body.password
+  const password = req.body.password;
   db.users.findOne({ where: { email: email } }).then(user => {
     if (user === null) {
-      res.render('login', { error_message: "Please check your username & password" })
+      res.render("login", {
+        error_message: "Please check your username & password"
+      });
     } else {
-      bcrypt.compare(password, user.passwordHash, function (err, matched) {
+      bcrypt.compare(password, user.passwordHash, function(err, matched) {
         if (matched) {
-          req.session.userId = user.id
-          const { id, name, email, photo_url } = user
-          res.render('userprofile', {
+          req.session.userId = user.id;
+          const { id, name, email, photo_url } = user;
+          res.render("userprofile", {
             id: id,
             name: name,
             email: email,
-            photo: photo_url,
+            photo: photo_url
           });
         } else {
-          res.render("login", { error_message: "Please check your username & password" })
+          res.render("login", {
+            error_message: "Please check your username & password"
+          });
         }
-      })
+      });
     }
-  })
-})
-app.post('/updatePassword', (req, res) => {
-  db.users.findOne({
-    where: {
-      id: req.session.userId,
-    }
-  }).then(user => {
-    if (user.name != undefined) {
-      bcrypt.hash(req.body.password, 10, function (err, hash) {
-        user.update({
-          passwordHash: hash,
-          resetPasswordToken: null
-        })
-      })
-      return user
-    }
-  }).then(user => {
-    req.session.userId = user.id
-    res.render('welcome', { isAuthenticated: true })
-  })
-})
+  });
+});
+app.post("/updatePassword", (req, res) => {
+  db.users
+    .findOne({
+      where: {
+        id: req.session.userId
+      }
+    })
+    .then(user => {
+      if (user.name != undefined) {
+        bcrypt.hash(req.body.password, 10, function(err, hash) {
+          user.update({
+            passwordHash: hash,
+            resetPasswordToken: null
+          });
+        });
+        return user;
+      }
+    })
+    .then(user => {
+      req.session.userId = user.id;
+      res.render("welcome", { isAuthenticated: true });
+    });
+});
 
 app.post("/upload", (req, res, next) => {
   if (Object.keys(req.files).length == 0) {
-    return res.status(400).send('No files were uploaded.');
+    return res.status(400).send("No files were uploaded.");
   }
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   let profilePic = req.files.profilePic;
-  let fileName = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  let fileName =
+    Math.random()
+      .toString(36)
+      .substring(2, 15) +
+    Math.random()
+      .toString(36)
+      .substring(2, 15);
   let user_id = req.session.user_id;
   const params = {
-    Bucket: 'chirperimages',
+    Bucket: "gifttoyoupics",
     Key: `${fileName}.jpeg`,
     Body: profilePic.data,
-    ACL: 'public-read'
-  }
-  s3.upload(params, function (Err, data) {
-    if (Err) throw Err
-    console.log(data.Location)//Logs the url to the s3 upload
+    ACL: "public-read"
+  };
+  s3.upload(params, function(Err, data) {
+    if (Err) throw Err;
+    console.log(data.Location); //Logs the url to the s3 upload
     db.users.findOne({ where: { id: user_id } }).then(user => {
       user.update({ photo_url: `${data.Location}` }).then(() => {
         res.json("Uploaded new pic");
-      })
-    })
+      });
+    });
   });
 });
 
 //!Server Port
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Server running on port 3000')
-})
-
-
-
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server running on port 3000");
+});
 
 //! Apollo set up
-const { ApolloServer, gql } = require('apollo-server-express');
-const { makeExecutableSchema } = require('graphql-tools');
-const typeDefs = require('./schema');
-const resolvers = require('./resolvers');
-models = db
-const apolloServ = new ApolloServer({ typeDefs, resolvers, context: { models } })
-apolloServ.applyMiddleware({ app })
+const { ApolloServer, gql } = require("apollo-server-express");
+const { makeExecutableSchema } = require("graphql-tools");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+models = db;
+const apolloServ = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: { models }
+});
+apolloServ.applyMiddleware({ app });
--- public/js/queryMethods.js
+++ public/js/queryMethods.js
@@ -1,19 +1,21 @@
-const url = `/graphql`
+const url = `/graphql`;
 const opts = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
+  method: "POST",
+  headers: { "Content-Type": "application/json" }
+};
+/*
+ * Implementation of a few GraphQL GET requests with fetch
+ * Use the playground tool @ localhost:3000/graphql to test your queries.
+ * Query Methods
+ */
 
-}
-//Implementation of a GraphQL GET request with fetch
-//Use the playground tool @ localhost:3000/graphql to test your queries.
-//* Query Methods
 /**
- * 
- * @param {Integer} id 
+ *
+ * @param {Integer} id :Requires the user primary key.
  * @returns {Array} :Returns an array of user likes with all of the information for the rendering.
  */
 async function getUserLikes(id) {
-    const query = `query{
+  const query = `query{
         getUserLikes(userId: ${Number(id)}){
           id
           listingId
@@ -24,18 +26,18 @@ async function getUserLikes(id) {
             name
             email
           }
-        }}`
-    opts.body = JSON.stringify({ query })
-    const response = await fetch(url, opts)
-    return response.json()
+        }}`;
+  opts.body = JSON.stringify({ query });
+  const response = await fetch(url, opts);
+  return response.json();
 }
 /**
- * 
- * @param {Integer} id Receives the primary key for a survey
+ *
+ * @param {Integer} id Requires the primary key for a survey
  * @returns {Array} : Returns an array of user surveys including the name & answers
  */
 async function getuserSurveys(id) {
-    const query = `query{
+  const query = `query{
         getUserSurveys(userId: ${id}){
         id
         name
@@ -44,106 +46,112 @@ async function getuserSurveys(id) {
           answer
         }
       }  
-    }`
-    opts.body = JSON.stringify({ query })
-    const response = await fetch(url, opts)
-    return response.json()
+    }`;
+  opts.body = JSON.stringify({ query });
+  const response = await fetch(url, opts);
+  return response.json();
 }
 
-
 //* Mutations
+
 /**
- * 
- * @param {Integer} listing 
- * @param {String} imgUrl 
- * @param {Integer} userId 
- * @param {String} title 
- * @param {Integer} price
+ *
+ * @param {Integer} listingId : Integer of the listing ID per Etsy's Page
+ * @param {String} imgUrl :String with the url of the listing
+ * @param {Integer} userId :PK of the user Integer
+ * @param {String} title :Tilte for the listing
+ * @param {Integer} price :Integer
  * @returns {String}
  */
 async function addUserLike(listing, imgUrl, userId, title, price) {
-    const query = `mutation{
-        createUserLike(listing: ${Number(listing)} imgUrl: "${imgUrl}" userId: ${Number(userId)} title: "${title}" price: ${Number(price)}){
+  const query = `mutation{
+        createUserLike(listing: ${Number(
+          listing
+        )} imgUrl: "${imgUrl}" userId: ${Number(
+    userId
+  )} title: "${title}" price: ${Number(price)}){
             id
         }
-    }`
-    //! Important to use an object when Stringifying the body
-    opts.body = JSON.stringify({ query })
-    const response = await fetch(url, opts)
-    return response.json()
+    }`;
+  //! Important to use an object when Stringifying the body
+  opts.body = JSON.stringify({ query });
+  const response = await fetch(url, opts);
+  return response.json();
 }
 
 /**
- * 
+ *
  * @param {Integer} id This is the primary key for the like.
  * @returns {String}
  */
 
 async function removeUserLike(id) {
-    const query = `mutation{
+  const query = `mutation{
         removeUserLike(id: ${Number(id)})
-    }`
-    opts.body = JSON.stringify({ query })
-    const response = await fetch(url, opts)
-    return response.json()
+    }`;
+  opts.body = JSON.stringify({ query });
+  const response = await fetch(url, opts);
+  return response.json();
 }
 /**
- * 
+ *
  * @param {String} name :Requires the first answer of the survey
  * @param {Integer} userId :Uses the primary key of the user for association
  * @returns {Object} :Returns an object of the newly created
  */
 async function createUserSurvey(name, userId) {
-    if (typeof name === String && typeof userId === Number) {
-        const query = `mutation{
+  if (typeof name === String && typeof userId === Number) {
+    const query = `mutation{
             createSurvey(name: "${name}", user_id: ${Number(userId)}){
                 name
                 id
             }
-        }`
-        opts.body = JSON.stringify({ query })
-        const response = await fetch(url, opts)
-        return response.json()
-    } else {
-        if (typeof name != String) {
-            console.log(`Error name is of type ${typeof name} not String!`)
-        } else if (typeof userId != Number) {
-            console.log(`Error name is of type ${typeof userId} not Number!`)
-        }
+        }`;
+    opts.body = JSON.stringify({ query });
+    const response = await fetch(url, opts);
+    return response.json();
+  } else {
+    if (typeof name != String) {
+      console.log(`Error name is of type ${typeof name} not String!`);
+    } else if (typeof userId != Number) {
+      console.log(`Error name is of type ${typeof userId} not Number!`);
     }
+  }
 }
+
 /**
- * 
+ *
  * @param {String} answer :String sepcifying the question
  * @param {String} question :User value submited
  * @param {Integer} surveyId :Integer representing the survey relation
  */
 async function createAnswer(answer, question, surveyId) {
-    const query = `mutation{
-        createAnswer(answer: "${answer}" question: "${question}" surveyId: ${Number(surveyId)} required: false){
+  const query = `mutation{
+        createAnswer(answer: "${answer}" question: "${question}" surveyId: ${Number(
+    surveyId
+  )} required: false){
               id
         }
-      }`
-    opts.body = JSON.stringify({ query })
-    const response = await fetch(url, opts)
-    return response.json()
+      }`;
+  opts.body = JSON.stringify({ query });
+  const response = await fetch(url, opts);
+  return response.json();
 }
 
 async function addSurveyCategory(name, surveyId) {
-    const query = `mutation{
-        createCategory(name: "${name}" surveyId: ${Number(surveyId)}){
-          survey{
-            answers{
-              question
-              answer
-            }
-            user{
-              name
-              email
-            }
-        }
-      }`
-    opts.body = JSON.stringify({ query })
-    const response = await fetch(url, opts)
-    return response.json()
+  const query = `mutation{
+    createCategory(name: "${name}" surveyId: ${Number(surveyId)}){
+        survey{
+        answers{
+                question
+                answer
+                }
+                user{
+                    name 
+                    email
+                    }} 
+    }`;
+  opts.body = JSON.stringify({ query });
+  const response = await fetch(url, opts);
+  return response.json();
 }
