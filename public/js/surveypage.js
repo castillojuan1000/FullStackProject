f62ddedf25b9922c9865b8e37dedd90a6b097a14
joetta25 <joetta.chasidy@gmail.com>
2019-08-31 16:30:55 -0400

    grabbed the values of the

--- public/js/surveypage.js
+++ public/js/surveypage.js
@@ -5,14 +5,10 @@
 //TODO: 3. Create the range  slider 
 //TODO: 4. Grab the data from each question, once the user answers.
 
-var questionNumber;
+var questionNumber= 1;
 
 window.addEventListener('DOMContentLoaded', function () {
    document.getElementById('start').addEventListener('click', function () {
-      questionNumber = 1;
-      document.getElementById('question1').classList.toggle('hide');
-      document.getElementById('answer1').classList.toggle('hide');
-      questionNumber++;
    });
 
    const survey = document.forms.namedItem('surveyForm');
@@ -38,42 +34,9 @@ window.addEventListener('DOMContentLoaded', function () {
 
 })
 function showNextQuestion() {
-   document.getElementById(`question${questionNumber}`).classList.toggle('hide');
-   document.getElementById(`answer${questionNumber}`).classList.toggle('hide');
+   document.getElementById(`form${questionNumber}`).classList.toggle('hide');
+   // document.getElementById(`answer${questionNumber}`).classList.toggle('hide');
    questionNumber++;
-}
-function showConsent() {
-   $('#answer1').show();
-   $('#answer2').show();
-   $('#answer3').show();
-   $('#answer4').show();
-   $('#answer5').show();
-   $('#answer6').show();
-   $('#answer7').show();
-   $('#answer8').show();
-   $('#answer9').show();
-   $('#answer10').show();
-   $('#answer11').show();
-   $('#answer12').show();
-   $('#answer13').show();
-   $('#answer14').show();
-   $('#answer15').show();
-   $('#question1').show();
-   $('#question2').show();
-   $('#question3').show();
-   $('#question4').show();
-   $('#question5').show();
-   $('#question6').show();
-   $('#question7').show();
-   $('#question8').show();
-   $('#question9').show();
-   $('#question10').show();
-   $('#question11').show();
-   $('#question12').show();
-   $('#question13').show();
-   $('#question14').show();
-   $('#question15').show();
-
 }
 //-----this is grabbing the data from the surveySET file on the backened 
 $.get('/surveyData').then(res => {
@@ -82,50 +45,92 @@ $.get('/surveyData').then(res => {
    const container = document.getElementById('surveyForm'); // this is grabbing the container on the HTMl 
    container.innerHTML = survey.map((Question, i)=>{  
       const{question,type,choices} = Question;
+      const questionNum = i
+      if(i != 14){
+         var buttonHTML = `<button type="submit" onclick="showNextQuestion()" class="btn btn-primary">Next</button>`
+      }else{
+         var buttonHTML = `<button type="submit" class="btn btn-primary">Submit</button>`
+      }
       //creating a if and else statment but using switch method 
       switch(type){
-         case 'checkbox': return renderCheckBox(Question,i+1)
+         case 'checkbox': return `
+         <form class="hide" id="form${questionNum}">
+         ${renderCheckBox(Question,questionNum)}
+         ${buttonHTML}
+         </form>
+         `
          break;
-         case 'range': return renderRangeButton(Question,i+1)
+         
+         case 'range': return `
+         <form class="hide" id="form${questionNum}">
+         ${renderRangeButton(Question,questionNum)}
+         ${buttonHTML}
+         </form>
+         `
          break;
-         case 'radio': return renderRadioButton(Question,i+1)
-         break
-         case 'text': return renderTextButton(Question,i+1)
-      }
 
+         case 'radio': return `
+         <form  class="hide" id="form${questionNum}">
+         ${renderRadioButton(Question,questionNum)}
+         ${buttonHTML}
+         </form>
+         `
+        
+         break
+         case 'text': return `
+         <form  class="hide" id="form${questionNum}">
+         ${renderTextButton(Question,questionNum)}
+         <button type="submit" onclick="showNextQuestion()" class="btn btn-primary">Next</button>
+         </form>
+         `
+      }
    }).join('')
+}).then(()=>{
+   //* Select all of the form childs inside of the id="surveyForm"
+   const forms = document.querySelectorAll('#surveyForm form'); // queryselectorAll prints out a array and then it needs to loop through each element 
+   forms.forEach((form)=>{
+      form.onsubmit= (e)=>{
+         e.preventDefault()
+         console.log(form.elements.answer.value);
+         if(form.querySelector(`[type="text"]`) != null){
+            const values = form.querySelectorAll('[type="text"]')
+            values.forEach(value => console.log(value.value))
+         }
+      }
+   })
+
 
 })
 
 //-----rendering each question type text , checkbox, radio , range 
 
-function renderCheckBox(Question,num){
+function renderCheckBox(Question,num){     
    const {question, type, choices}= Question;
    var checkBoxHTML = choices.map((choice)=> {
       return `
       <div>
-      <input class="form-check-input" type="checkbox" value="Female" id="firstbox">
+      <input class="form-check-input" type="checkbox" value="Female" name="answer" id="firstbox">
       <label class="form-check-label" for="firstbox">
-      ${choice} 
+      ${choice}
       </label>
       </div>
       `
    })
    return `
    <div class="row">
-         <div class="col hide" id="question${num}">
+         <div class="col" id="question${num}">
             <h2>${question}</h2>
 
           </div>
    </div>
       <br />
    <div class="row">
-         <div class="col hide" id="answer${num}" >
+         <div class="col" id="answer${num}" >
          <div class="form-check">
          ${checkBoxHTML.join('')}
          </div>
          <br />
-         <button type="button" onclick="showNextQuestion()" class="btn btn-primary">Next</button>
+         
        </div>
    </div>`
 
@@ -135,7 +140,7 @@ function renderRadioButton(Question,num){
    const{question,type,choices}= Question;
    var radioButtonHTML = choices.map((choice)=> {
       return ` <div class=radioBox>
-                <input type="${type}" value="${choice}"></input>
+                <input type="${type}" value="${choice}" name="answer"></input>
                 </div>
                 <div>
                 ${choice}
@@ -144,19 +149,19 @@ function renderRadioButton(Question,num){
    })
    return `
    <div class="row">
-            <div class="col hide" id="question${num}">
+            <div class="col" id="question${num}">
                 <h2>${question}</h2>
             </div>
         </div>
         <br />
         <div class="row">
-            <div class="col hide" id="answer${num}">
+            <div class="col" id="answer${num}">
             <div class="d-flex flex-column w-25 mx-auto">
                 ${radioButtonHTML.join('')}
                 </div>
                 <br />
                 <br />
-                <button type="button" onclick="showNextQuestion()" class="btn btn-primary">Next</button>
+                
              </div>
         </div>
         <br /> 
@@ -167,26 +172,22 @@ function renderTextButton(Question,num){
    const{question,type,choices}= Question;
    var textButtonHTML = choices.map((choice)=>{
       return `
-          ${choice}: <input type="${type}" name="${choice}" value=""><br>
-                 
+          <input type="${type}" name="answer" placeholder="${choice}">   
       `
    })
    return `
    <div class="row">
-            <div class="col hide" id="question${num}">
+            <div class="col" id="question${num}">
                 <h2>${question}</h2>
             </div>
         </div>
         <br />
         <div class="row">
-            <div class="col hide" id="answer${num}">
-                    <form action=" ">  <!-- Create a new route  -->
-                       ${textButtonHTML.join('')}
+            <div class="col" id="answer${num}">  <!-- Create a new route  -->
+                       ${textButtonHTML.join('<br/>')}
                         <br />
                         <br />
                         <br />
-                        <button type="button" onclick="showNextQuestion()" class="btn btn-primary">Next</button>
-                    </form>
             </div>
         </div>
         <br />
@@ -197,25 +198,50 @@ function renderRangeButton(Question,num){
    const{question,type,choices}= Question;
    var rangeButtonHTML = choices.map((choice)=>{
       return `
-          <input type="${type}" min="10" max="1000" step="5" value="" data-orientation="vertical"></input>
+          <input type="${type}" min="10" max="1000" step="5"  name="answer" value="" data-orientation="vertical"></input>
       `
    })
    return `
     <div class="row">
-            <div class="col hide" id="question${num}">
+            <div class="col" id="question${num}">
                 <h2>${question}</h2>
             </div>
         </div>
         <br />
         <div class="row">
-            <div class="col hide" id="answer${num}">  <!-- Create a new route  -->
+            <div class="col" id="answer${num}">  <!-- Create a new route  -->
                 ${rangeButtonHTML.join('')}
                 <br />
                 <br />
-                <button type="button" onclick="showNextQuestion()" class="btn btn-primary">Next</button>
+                
             </div>
         </div>
         <br />
    
    `
 }
+
+
+// adding a eventlistener to the buttons on the questions to grab the values  
+
+// button1.addEventListener("click", function(){
+//    const{question,type,choices};
+//    document.querySelector(type).value;
+
+// })
+// button2.addEventListener("click", function(){
+//    const{question,type,choices};
+//    document.querySelector(type).value;
+// })
+// button3.addEventListener("click", function(){
+//    const{question,type,choices};
+//    document.querySelector(type).value;
+//    console.log(document.querySelector(type).value)
+
+   
+// })
+// button4.addEventListener("click", function(){
+//    const{question,type,choices};
+//    document.querySelector(type).value;
+   
+// })
