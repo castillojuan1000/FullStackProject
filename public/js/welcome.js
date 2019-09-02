5623724de0ac938083f8892e5037529fbc0c470c
Alexander Santos <alexsantosantana@live.com>
2019-09-02 11:57:30 -0400

    CSS MediaQueries for responsiveness

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -2,26 +2,34 @@
 const giftReceiver = document.querySelector('#giftReceiver')
 const receivers = ['Dad', 'Mom', 'Sister', 'Brother', 'Friend', 'Significant Other']
 var loopCompleted = 0;
-
-anime({
-    targets: '#giftReceiver',
-    translateY: 50,
-    direction: 'reverse',
-    opacity: 0,
-    duration: 1500,
-    delay: 2500,
-    loop: true,
-    loopBegin: function (anim) {
-        giftReceiver.textContent = `${receivers[loopCompleted]}`
-    },
-    loopComplete: function (anim) {
-        if (loopCompleted === receivers.length - 1) {
-            loopCompleted = 0
-        } else {
-            loopCompleted++
+try {
+    anime({
+        targets: '#giftReceiver',
+        translateY: 50,
+        direction: 'reverse',
+        opacity: 0,
+        duration: 1500,
+        delay: 2500,
+        loop: true,
+        loopBegin: function (anim) {
+            giftReceiver.textContent = `${receivers[loopCompleted]}`
+        },
+        loopComplete: function (anim) {
+            if (loopCompleted === receivers.length - 1) {
+                loopCompleted = 0
+            } else {
+                loopCompleted++
+            }
         }
-    }
-})
+    })
+} catch (error) {
+    console.log(error);
+}
+
+
+
+
+
 
 
 
