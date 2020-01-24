9297345c6a2f17215cbade7a91c77109b20521c7
Alexander Santos <alexsantosantana@live.com>
2020-01-24 16:44:26 -0500

    Working on adding more heroku deployment support

--- config/config.js
+++ config/config.js
@@ -1,23 +1,20 @@
 module.exports = {
-  "development": {
-    "username": process.env.DBUSER,
-    "password": process.env.DBPASS,
-    "database": "fullstackproject",
-    "host": process.env.DBURL,
-    "dialect": "postgres",
-    "operatorsAliases": false,
-    "logging": false
-  },
-  "production": {
-    "username": process.env.DBUSER,
-    "password": process.env.DBPASS,
-    "database": "fullstackproject",
-    "host": process.env.DBURL,
-    "dialect": "postgres",
-    "operatorsAliases": false,
-    "logging": false
-  }
-
-
-}
-
+	development: {
+		username: process.env.PG_USER || 'postgres',
+		password: process.env.PG_PASSWORD || null,
+		database: 'gifttoyou',
+		host: '127.0.0.1',
+		dialect: 'postgres',
+		logging: false
+	},
+	production: {
+		use_env_variable: process.env.DATABASE_URL,
+		host: process.env.DATABASE_URL,
+		dialect: 'postgres',
+		operatorsAliases: false,
+		logging: false,
+		dialectOptions: {
+			ssl: true
+		}
+	}
+};
